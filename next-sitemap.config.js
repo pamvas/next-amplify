cd4662663e928074beb0e231914a94ad225e4e3a
Pamela <pamvp@amazon.com>
2022-12-01 16:56:55 -0600

    second

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 // Default code you can customize according to your requirements.
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://example.com',
-    generateRobotsTxt: true, // (optional)
-    // REST CODE READ DOCS  ...
-    }
+const config = {
+    siteUrl: "www.pamvp-test.com",
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: "*",
+          allow: "/",
+        },
+      ],
+  
+      // additionalPaths: async (config) => [
+      //   await config.transform(config, "/comments"),
+      // ],
+     
+    },
+  };
+   
+  module.exports = config;
